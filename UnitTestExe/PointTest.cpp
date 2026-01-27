31bdf692c82a83bae6ccbb3b65a146ba482a52ab
sbroger <stefan.roger1@gmail.com>
2026-01-26 20:25:18 -0800

    Fix build issue.

--- UnitTestExe/PointTest.cpp
+++ UnitTestExe/PointTest.cpp
@@ -2,8 +2,10 @@
 
 #include "Point.h"
 
+// Remove this test once you've implemented real content for the Point class.
 TEST(Point, PlaceholderApi)
 {
-  Point x;
-  x.PlaceholderApiFunction();
+    Point x;
+    x.PlaceholderApiFunction();
+    CHECK(true);
 }
